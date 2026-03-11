ec8096e8dbbed8c70ae31296cd34801989981f5b
nikaa-dev <chea.nikaa@gmail.com>
2026-03-11 01:23:57 -0700

    toggle add user in user management

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bfc3ce8ec81f5ec8c4d3c1131c4196ad8c3f0a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda020cb461e877c1f0cfb18f465a41b0fa835ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
